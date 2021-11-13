e7dc301592ed45a368e8f2940ae96d6cea63365e
Isabelle Villegas <izzy57467@gmail.com>
2021-11-13 14:31:09 -0600

    Added Xcode Project

---
+++ Grocery Buddy/Grocery Buddy/Grocery_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_BuddyApp.swift
+//  Grocery Buddy
+//
+//  Created by Isabelle Villegas on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
